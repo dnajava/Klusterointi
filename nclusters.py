b93d22032f21d02dc7153500f0905e444c59b887
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-02-22 05:41:27 +0200

    Add files via upload

--- nclusters.py
+++ nclusters.py
@@ -31,7 +31,6 @@ class nclusters:
             i += 1
         return i
 
-
 # === Show or output clusters
 
     def show(self):
@@ -108,7 +107,6 @@ class nclusters:
                     print('And ', end=''),
                 print(unknown, 'unknown MDKAs')
 
-
     def amount_unknown_mdkas(self, cluster_p=None):
         i = 0
         if cluster_p == None:
@@ -130,7 +128,6 @@ class nclusters:
                             unknown += 1
                 i = i+1
             return unknown
-
 
     def write_gephi_sources(self):
         # Write nodes.csv and links.csv
@@ -206,7 +203,6 @@ class nclusters:
             print('</cluster>')
         print('</clusters>')                                 # End of XML
 
-
 # === Cluster operations
 
     def sort_mdkas(self):
@@ -235,7 +231,6 @@ class nclusters:
                         print('Same match: ', m1_p[1], ' ', m2_p[2], ' !!')
                     return True
         return False
-
 
     @staticmethod
     def samecluster(clu1_p, clu2_p, debug1=False):
@@ -273,14 +268,13 @@ class nclusters:
 
     def delete_duplicates(self, debug=False):
         # https://stackoverflow.com/questions/1388818/how-can-i-compare-two-lists-in-python-and-return-matches
-        # Do you want sorting? Sort cluster_p, sort clusters in nclusters if you are not using above method.
         # FIXME: Check again if right duplicate clusters are deleted.
 
         found = False
         if(debug):
             print('Nclusters delete_duplicates()')
 
-        if len(self.nclusters) < 1:                      # There is 1 or 0 clusters
+        if len(self.nclusters) < 1:                      # There are only 1 or 0 clusters
             return False
 
         ind1 = 0
@@ -295,8 +289,8 @@ class nclusters:
                     found = True
                     if debug:
                         print('Clusters are similair! Before popping lenght =', len(self.nclusters))
-                        self.show_mdkas(ind1)
-                        self.show_mdkas(ind2)
+                        # self.show_mdkas(ind1)
+                        # self.show_mdkas(ind2)
 
                     # Cluster to be deleted can contain matches, which original cluster don't have
                     self.copy_extra_matches(clu, clu2)
@@ -321,19 +315,48 @@ class nclusters:
         else:
             return False
 
-    def to_be_splitted(self, debug=False):
-        # TODO: Add code to search clusters, which need split
-        # One method is taking a pair clusters and check if they have just same matches.
+    def split_cluster(self, clu_original_p, clu_check_p):
+        # Add matches to new cluster. And new cluster to clusters list.
+        # FIXME: Test, if this split cluster works.
 
-        # Second method is to take one tested and compare it's matches to all other tested. If all tested have same
-        # GD values to all cluster matches, split is saturated. If not, there is something to split
+        new_clu = mclusters()
 
+        for m in clu_check_p:
+            for m2 in clu_original_p:
+                if m != m2:
+                    new_clu.append(m)
+
+        if len(new_clu) < 1:
+            return None
+        else
+            return new_clu
+
+    def compare_cluster_pair_to_split(clu1_p, clu2_p, debug=False):
+        # TODO: Add code to compare if two clusters are GD uniform or not.
+        #  If they are not, move extra matches to new cluster
         return False
 
-        found = False
+    def split_clusters(self, debug=False):
+        # This is maybe most complicated in nclusters and in wholw program.
+        # You may take a big cup of good coffee. And sit down and think.
+
+        # One method: Find all common matches between two clusters. Split other matches to new cluster.
+        # Do that to every cluster pairs (takes n ** 2 cluster pair comparisons). When there is nothing to split,
+        # splitting is done and saturated. Sounds good or best of not so good methods. :)
+        # Split only if there are common uniform set of matches which are in both cluster. If you split
+        # strange clusters, it's infinite loop. And don't add new cluster to list. It can cause strange
+        # situation. Add when the loop has run out.
+
+        # Second method: Take a match in some cluster. And search it from other clusters. If found, look that
+        # there all is same GD from original cluster. Hmmm....
+
+        # One method is to take one tested and compare it's matches to all other tested. If all tested have same
+        # GD values to all cluster matches, split is saturated. If not, there is something to split
+
         if(debug):
             print('Nclusters to_be_splitted()')
 
+        found = False
         ind1 = 0
         for clu in self.nclusters:
             ind2 = ind1 + 1
@@ -343,13 +366,9 @@ class nclusters:
                     print(clu, '\n', clu2)
                 # Test if the two cluster have matches which have different GD-value than others.
                 # So, they belongs to other new cluster
+                if compare_cluster_pair_to_split(clu, clu2, True):
+                    return True
         return False
-
-    @staticmethod
-    def split_cluster():
-        # TODO: Add code to split cluster when needed
-        return False
-
 
 # Network operations
 
