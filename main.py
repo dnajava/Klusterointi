72cb2d07f4ed25a03d0c1b594801c185542915a6
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-02-27 00:43:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 __author__ = "Ilpo Kantonen"
 __date__ = "$20.1.2021 2:01:51$"
-# Cluster network program. It makes nodes.csv and links.csv to Gephi. Gephi can display a network of mt-dna matches
-# in one haplogroup. The match clusters can be subgroups to that haplogroup. Grouping depends on GD values between
-# matches. You can also print MDKA:s as txt and xml file and a spreadsheet.
+"""
+Cluster network program. It makes nodes.csv and links.csv to Gephi. Gephi can display a network of mt-dna matches
+in one haplogroup. The match clusters can be subgroups to that haplogroup. Grouping depends on GD values between
+matches. You can also print MDKA:s as txt and xml file and a spreadsheet.
 
-# This is the main purpose to this program. You can do other things with this program too. Be free to modify code.
-# If you think you know better methods to do something, feel free to contact and tell to Ilpo at ilpo@iki.fi.
+This is the main purpose to this program. You can do other things with this program too. Be free to modify code.
+If you think you know better methods to do something, feel free to contact and tell to Ilpo at ilpo@iki.fi.
 
-# Input to this program are downloaded mt-dna match lists from FTDNA. Output are nodes.csv and links.csv. With Gephi
-# you can do beautiful graphs of GD network.
+Input to this program are downloaded mt-dna match lists from FTDNA. Output are nodes.csv and links.csv. With Gephi
+you can do beautiful graphs of GD network.
 
-# Version 0.1.1.
+Version 0.2.0.
+"""
 
 from kit import kit
 import nclusters
@@ -46,11 +48,8 @@ if __name__ == '__main__':
         print('Splitted', sint, ' clusters.')
 
     # netclusters.write('U8a1a1b1_splitted.json')
-
-#    netclusters.show_mdkas()                            # Print all clusters (MDKAs)
-
+    # netclusters.show_mdkas()                          # Print all clusters (MDKAs)
     # netclusters.mk_txt(1)                             # Print one cluster (MDKAs)
     # netclusters.mk_xml()                              # Print in XML form
-
     # netclusters.write_gephi_sources()                   # Write nodes.csv and links.csv to Gephi
 
