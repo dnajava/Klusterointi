4ff135ae2d3288173b37fa6fa46d5bcf0dcf2735
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-03-01 23:35:36 +0200

    Add files via upload

--- mclusters.py
+++ mclusters.py
@@ -1,35 +1,30 @@
-# MCluster is Match Cluster of some Kit (tested person)
+"""
+Match Clusters of one Kit (tested person)
+"""
 from csv import reader
 
 class mclusters:
+    haplogroup = ''                                 # Haplogroup of samples
+    name = ''                                       # Subroup of haplogourp. A cluster of haplogroup.
+    gdmax = 4                                       # FTDNA lists only GD 0 - 3 matches
     gd0 = []
     gd1 = []
     gd2 = []
     gd3 = []
-    gd = [gd0, gd1, gd2, gd3]
+    gds = [gd0, gd1, gd2, gd3]
 
-    def __init__(self):
-        self.gdmax = 4
-        self.gd0 = []
-        self.gd1 = []
-        self.gd2 = []
-        self.gd3 = []
-        self.gd = [self.gd0, self.gd1, self.gd2, self.gd3]
+    def __init__(self, haplogroup_p=''):
+        if haplogourp_p != '':
+            haplogroup = haplogroup_p
 
-    def __init__(self,name_p,fname_p):
-        self.gdmax = 4
-        self.gd0 = []
-        self.gd1 = []
-        self.gd2 = []
-        self.gd3 = []
-        self.gd = [self.gd0, self.gd1, self.gd2, self.gd3]
+    def __init__(self,haplogroup_p='', name_p='', fname_p=''):
+        self.haploroup = haplogroup_p
         self.name = name_p
         self.fname = fname_p
-        # TODO: Code this more clever, if you can.
 
     def show(self, debug2_p = False, debug3_p = False):
         i = 0
-        for y in self.gd:
+        for y in self.gds:
             if debug2_p:
                 print('GD', i, 'cluster and there are', len(y), 'matches.')
                 i += 1
@@ -53,25 +48,25 @@ class mclusters:
             if self.gd3 != None:
                 return self.gd3
 
-
-    def read_clusters(self, fname_p, pname_p):
+    def read_kit_clusters(self, fname_p, pname_p):
         ind = 0
         matches = []
         read_obj = None
 
-        # You have to add kit's owner to first cluster. Why? Read documents.
-        self.gd0.append(('0',pname_p,'','','','','','',''))                       # Add as a tuple
+        self.gd0.append(('0',pname_p,'','','','','','',''))     # Matchlist doesn't contain kits owner. Add him/her.
+        # TODO: Add the rest data to first only name cluster, if it possible.
 
         try:
             with open(fname_p, 'r') as read_obj:
                 csv_reader = reader(read_obj)
                 for m in csv_reader:
                     if ind > 0:
+                        # print('read kit clusters: ', m)
                         matches.append(tuple(m))
                     ind += 1
         except (IOError, OSError) as err:
             print(err)
-            return []
+            return             # []
         finally:
             if read_obj is not None:
                 read_obj.close()
