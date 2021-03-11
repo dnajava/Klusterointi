d3fda3dffd4483bc066b5e0ef05dab0aa360aa42
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-03-11 21:39:01 +0200

    Add files via upload

---
+++ mtsettings.py
@@ -0,0 +1,9 @@
+'''
+Settings of mt_network programs.
+'''
+
+GDMAX = 4
+KITSFILE = 'kits.csv'
+DLDIR = '/home/user/Downloads/'
+HAPLOGROUP ='A1b2c3'
+OUTPUTDIR = '/home/user/PycharmProjects/mt_network/'
