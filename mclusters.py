3ca0638f070125acfd1be950b79c3c374323ea6f
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-03-14 18:10:15 +0200

    Add files via upload

--- mclusters.py
+++ mclusters.py
@@ -29,20 +29,20 @@ class Basematch:
         print('Match:', self.kit, ' ', self.gd, ' ', self.Fullname, ' ', self.Email, ' ', self.MDKA)
 
 class Filematch(Basematch):
-    def __init__(self, kit_p, gd_p, fun, fin, min, lan, email_p, mdka_p):
+    def __init__(self, kit_p, gd_p, fun_p, fin_p, min_p, lan_p, email_p, mdka_p):
         """
         :param kit_p:
         :param gd_p:
-        :param fun:
-        :param fin:
-        :param min:
-        :param lan:
+        :param fun_p:
+        :param fin_p:
+        :param min_p:
+        :param lan_p:
         :param email_p:
         :param mdka_p:
         """
         self.kit = kit_p
         self.gd = gd_p
-        super().__init__(fun, fin, min, lan, email_p, mdka_p)
+        super().__init__(fun_p, fin_p, min_p, lan_p, email_p, mdka_p)
 
 
 class Clumatch(Basematch):
@@ -73,16 +73,14 @@ class Mcluster:
         """
         self.kit_id = kit_id_p                      # Id of kit
         self.name = name_p                          # Name of subroup. A cluster of haplogroup.
-        self.gds = [[], [], [], []]                 # 4 levels of matches grouped by GD
-
+        self.gds = [[], [], [], []]                 # 4 levels of matches grouped by GD. Use Gds instead this!
 
     def show(self, debug2_p=False, debug3_p=False):
         """
+        Shows data of one match cluster
         :type debug2_p: bool
         :type debug3_p: bool
         """
-
-#        print('mcluster.py show Begin')
         i = 0
         for y in self.gds:
             if debug2_p:
@@ -93,22 +91,17 @@ class Mcluster:
             i += 1
         print('')
 
-    def show_links(self):
-        i = 0
-        for v in self.links:
-            print(v)
-
     def get_cluster(self, level=0) -> list:
         """
+        Returns a match cluster.
         :type level: int
-        :return: list
+        :return: list: list List of matches in one cluster
         """
         if 0 <= level < GDMAX-1:
-            if self.gds[level] != None:
+            if self.gds[level] is not None:
                 return self.gds[level]
         else:
             print('Wrong GD-level', level)
-
 
     def add_link(self, clu_p, gd_dest):
         """
@@ -122,7 +115,7 @@ class Mcluster:
     def add_default_links(self, kitclu_p, ind_p):
         self.links = []
         for i in range(1, 3):
-            self.links.append(Link(ind_p,ind_p+i,i))
+            self.links.append(Link(ind_p, ind_p+i, i))
 
     def show_links(self):
         print('Mclusters show_links')
@@ -130,17 +123,16 @@ class Mcluster:
             for li in self.links:
                 print('Link ', li.orig, '-', li.dest)
 
-
     def read_kit_clusters(self, kit_id_p: str, pname_p: str, fname_p: str) -> list:
         ind = 0
-        tmp_gds = [[], [], [], []]
+        tmp_gds = [[], [], [], []]   # Use Gds instead this!
 
         try:
             with open(fname_p, 'r') as read_obj:
                 csv_reader = reader(read_obj)
                 for m in csv_reader:
                     if ind == 0:
-                        new_match = Basematch(kit_id_p, 0, pname_p, '', '', '', '', '') # bogus match, we don't know
+                        new_match = Basematch(kit_id_p, 0, pname_p, '', '', '', '', '')  # bogus match, we don't know
                         self.gds[0].append(new_match)
                     else:
                         # DataFormats.txt     kit       gd         fun   fin   min   lam   email mdka
@@ -152,12 +144,8 @@ class Mcluster:
             return []
         finally:
             if read_obj is not None:
-               read_obj.close()
-#        print('mclusters mcluster read_kit_clusters: Here is kit', kit_id_p, 'and matches found grouped by clusters.')
-#        show_gds(tmp_gds)
+                read_obj.close()
         return tmp_gds
-
-
 
 # Non member methods / functions
 
@@ -172,12 +160,9 @@ def is_adjacent(cluster1_p, cluster2_p) -> bool:
         True if mname_p is in cluster_p.
     """
     same = 0                                            # How many same matches there are in cluster1_p
-
     for m in cluster1_p.Nclusters:
         for m2 in cluster2_p.Nclusters:
             if m[1] == m2[1]:                           # Are names of matches same?
                 same += 1
-
     result = False
     return result
-
