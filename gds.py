b72c226ab5c79d2c1cbe250a143a8ba6015cfd25
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-03-14 20:45:23 +0200

    Add files via upload

--- gds.py
+++ gds.py
@@ -1,15 +1,20 @@
 """
 GD levels of one kit grouped by GD.
 """
-
 from mtsettings import GDMAX
 
 class Gds:
-    links = []                                      # List of tuples: links to other clusters with GD distance
-
-    def ___init___(self):
+    def ___init___(self, level_p: int, value_p=None):
+        """
+        Creates new Gds with one match cluster to some level or only a new empty Gds.
+        :param level_p:
+        :param value_p:
+        :return:
+        """
         self.gd0, self.gd1, self.gd2, self.gd3 = [], [], [], []
         self.gds = [self.gd0, self.gd1, self.gd2, self.gd3]
+        if value_p is not None and 0 <= level_p < GDMAX:
+            self.gds[level_p] = value_p
 
     def add(self, idx, value):
         self.gds[idx].append(value)
