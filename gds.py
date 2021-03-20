470d46bc4f77c845e87913110a3616350b8914d8
Ilpo Kantonen <ilkant@users.noreply.github.com>
2021-03-20 02:26:39 +0200

    Add files via upload

--- gds.py
+++ gds.py
@@ -4,6 +4,8 @@ GD levels of one kit grouped by GD.
 from mtsettings import GDMAX
 
 class Gds:
+    gd0, gd1, gd2, gd3 = [], [], [], []
+    gds = [gd0, gd1, gd2, gd3]
     def ___init___(self, level_p: int, value_p=None):
         """
         Creates new Gds with one match cluster to some level or only a new empty Gds.
@@ -19,13 +21,32 @@ class Gds:
     def ___getitem___(self, i: int):
         return self.gds[i]
 
-    def add(self, idx, value):
-        self.gds[idx].append(value)
+    def add(self, i, value_p) -> bool:
+        if self.gds is None:
+            return False
+        if 0 <= i < GDMAX:
+            self.gds[i].append(value_p)
+            return True
+        else:
+            print('Error: Gds-index out of range.')
+            return False
 
     def show(self, i=0):
         if 0 <= i < GDMAX:
-            print('GD', i, self.gds[i])
+            if len(self.gd0) > 0:
+                for m in self.gd0:
+                    m.show()
+
+            if len(self.gd0) > 0:
+                for m in self.gd1:
+                    m.show()
+
+            if len(self.gd0) > 0:
+                for m in self.gd2:
+                    m.show()
+
+            if len(self.gd0) > 0:
+                for m in self.gd3:
+                    m.show()
         else:
-            for a in self.gds:
-                for i in range(0, GDMAX, 1):
-                    print('GD ', i, a)
+            print('Gds Error: Index out of range.')
