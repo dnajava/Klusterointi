745341692d8f05368e4c7e6bfdfb5a0f1d66edb9
Ilpo Kantonen <ilkant@users.noreply.github.com>
2025-10-16 00:22:13 +0300

    Update mtsettings.py
    Update.

--- mtsettings.py
+++ mtsettings.py
@@ -1,7 +1,9 @@
-# Settings of mt_network programs.
-
+'''
+Ohjelman asetukset. Näitä voi käyttää useampi metodi ohjelmassa.
+'''
 GDMAX = 4
 KITSFILE = 'kits.csv'
 DLDIR = '/home/user/Downloads/'
 HAPLOGROUP = 'A1b2c3'
 OUTPUTDIR = '/home/user/mt_network/'
+FENCODING = "utf-8"
